0e107f7a4be9dd7759a3fe4faf254e00ff77e084
Kyosuke Tomoda <tomoda.kyousuke@ci.i.u-tokyo.ac.jp>
2014-06-22 14:30:05 +0900

    仕様8を実�

--- Code1/test1.java
+++ Code1/test1.java
@@ -5,32 +5,40 @@ import java.io.*;
 public class Test1 {
     
 	public static void main(String args[]) {
-		String fileName = "recipe-data.txt";
+        int N = args.length;
+        String[] userName = new String[N/2];
+        String[] fileName = new String[N/2];
+        for (int i = 0; i<N/2; i++){
+            userName[i] = args[i*2];
+            fileName[i] = args[i*2+1];
+        }
 		String[] recipeList = null;
         int recipeId = -1; //レシピのidが選択されない場合はrecipeId = -1
-        if (args.length > 0) recipeId = Integer.parseInt(args[0]);
-		try{
-            File ANSIFile = new File(fileName);
-            FileReader fileReader = new FileReader(ANSIFile);
-            
-            int ch;
-            String readedRecipe = "";
-            while((ch = fileReader.read()) != -1){
-		        readedRecipe += (char)ch;
+        int recipeCount = 0;
+        if (N%2==1) recipeId = Integer.parseInt(args[N-1]);
+        for (int i=0; i<N/2; i++){
+            System.out.println("ユーザ名: " + userName[i]);
+            try{
+                File ANSIFile = new File(fileName[i]);
+                FileReader fileReader = new FileReader(ANSIFile);               
+                int ch;
+                String readedRecipe = "";
+                while((ch = fileReader.read()) != -1){
+                    readedRecipe += (char)ch;
+                }
+                recipeList = readedRecipe.split("\n");
+                fileReader.close();
+            }catch(FileNotFoundException e){
+                System.out.println(e);
+            }catch(IOException e){
+                System.out.println(e);
             }
-            recipeList = readedRecipe.split("\n");
-            fileReader.close();
-        }catch(FileNotFoundException e){
-            System.out.println(e);
-        }catch(IOException e){
-            System.out.println(e);
+            for (int j=0; j<recipeList.length; j++){
+                recipeCount++;
+                if (recipeId == -1 || recipeId == recipeCount){ //idの選択が行われていないか, または選択したidを表すとき
+                    System.out.println(recipeCount+": "+recipeList[j]); 
+                }
+            }
         }
-		for(int i=0; i<recipeList.length; i++)
-		{
-            if (recipeId == -1 || recipeId == i+1){ //idの選択が行われていないか, または選択したidを表すとき
-                System.out.println(i+1+": "+recipeList[i]); 
-            }
-            
-		}
-	}
+    }
 }

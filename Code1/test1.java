e9c57c1f18e647add84efe62ec0085d42e35c99c
ShunNomura <negimashimashi@gmail.com>
2014-06-22 12:03:36 +0900

    add listNum

--- Code1/test1.java
+++ Code1/test1.java
@@ -7,20 +7,27 @@ import java.io.IOException;
 public class test1 {
 	public static void main(String args[]) {
 		String fileName = "recipe-data.txt";
+		String[] recipeList = null;
 		try{
 		      File utf8File = new File(fileName);
 		      FileReader fileReader = new FileReader(utf8File);
 
 		      int ch;
+		      String readedRecipe = "";
 		      while((ch = fileReader.read()) != -1){
-		        System.out.print((char)ch);
+		        readedRecipe += (char)ch;
 		      }
-
+		      recipeList = readedRecipe.split("\n");
+		      
 		      fileReader.close();
 		    }catch(FileNotFoundException e){
 		      System.out.println(e);
 		    }catch(IOException e){
 		      System.out.println(e);
 		    }
+		for(int i=0; i<recipeList.length; i++)
+		{
+			System.out.println(i+1+": "+recipeList[i]);
+		}
 	}
 }

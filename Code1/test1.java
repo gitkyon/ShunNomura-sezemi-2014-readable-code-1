f182bbed728c43a5faf2be94cb2b8be52b64c7c3
ShunNomura <negimashimashi@gmail.com>
2014-06-22 11:29:25 +0900

    add readingRecipeData

--- Code1/test1.java
+++ Code1/test1.java
@@ -1,6 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+/**ファイルからレシピ情報を取得し、コンソールに出力*/
 public class test1 {
 	public static void main(String args[]) {
-		System.out.println("オムライス");
+		String fileName = "recipe-data.txt";
+		try{
+		      File file = new File(fileName);
+		      FileReader fileReader = new FileReader(file);
+
+		      int ch;
+		      while((ch = fileReader.read()) != -1){
+		        System.out.print((char)ch);
+		      }
+
+		      fileReader.close();
+		    }catch(FileNotFoundException e){
+		      System.out.println(e);
+		    }catch(IOException e){
+		      System.out.println(e);
+		    }
 	}
 }

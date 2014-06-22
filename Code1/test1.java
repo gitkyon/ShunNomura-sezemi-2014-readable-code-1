6b65bd2eb6e24ee2ecd183e740b5e364fd317c36
Kyosuke Tomoda <tomoda.kyousuke@ci.i.u-tokyo.ac.jp>
2014-06-22 14:02:11 +0900

    仕様6, 7のコメントを追加

--- Code1/test1.java
+++ Code1/test1.java
@@ -7,7 +7,7 @@ public class Test1 {
 	public static void main(String args[]) {
 		String fileName = "recipe-data.txt";
 		String[] recipeList = null;
-        int recipeId = -1;
+        int recipeId = -1; //レシピのidが選択されない場合はrecipeId = -1
         if (args.length > 0) recipeId = Integer.parseInt(args[0]);
 		try{
             File ANSIFile = new File(fileName);
@@ -27,8 +27,8 @@ public class Test1 {
         }
 		for(int i=0; i<recipeList.length; i++)
 		{
-            if (recipeId == -1 || recipeId == i+1){
-                System.out.println(i+1+": "+recipeList[i]);
+            if (recipeId == -1 || recipeId == i+1){ //idの選択が行われていないか, または選択したidを表すとき
+                System.out.println(i+1+": "+recipeList[i]); 
             }
             
 		}
